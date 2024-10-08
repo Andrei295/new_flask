e3159c3c7465497b498b6b48ade522148949b26c
Andrei <aandreia0807@gmail.com>
2024-10-08 11:55:31 +0100

    added another test

--- test/test.py
+++ test/test.py
@@ -3,14 +3,34 @@ import unittest
 from unittest import TestCase
 from cs50 import SQL
 
-data = SQL("sqlite:///C://Temp//userdata.db")
 
-class Test(unittest.TestCase): #python -m unittest
-    def test_value(self):
-        self.assertEqual(value, 1)
+
+def add(a, b):
+    return a + b
+
+
+class TestMath(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(add(2, 3), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
 
 
-value = data.execute("SELECT * FROM users WHERE ClassID = ?", 1)
+
+data = SQL("sqlite:///C://Temp//userdata.db")
+
+def get_name(user_1):
+    user_1 = data.execute("SELECT Name FROM users WHERE Username = Andrei567")
+
+    return user_1
+
+
+def adduser(user_1):
+    def test_user(self):
+        self.assertEqual(get_name(user_1), "Andrei")
+
+
+if __name__ == '__main__':
+    unittest.main()
