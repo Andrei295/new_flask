d98ce81317cecb1b4a89c19715e0e18edb03692f
Andrei <aandreia0807@gmail.com>
2024-11-05 12:07:37 +0000

    removed uneeded punctuation

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app) 
 
 
-@app.route("/",) #Used as a default (login / register) page
+@app.route("/") #Used as a default (login / register) page
 def index():
         return render_template("index.html")
 
